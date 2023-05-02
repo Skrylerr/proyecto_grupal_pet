b0cf9988a5919746b4fd90cdf61988e473d3e56a
Jesus Tang <jesutang3@gmail.com>
2023-05-01 20:39:00 -0500

    Mapa con autocompletado de direcciones

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,49 @@
-import './App.css';
-import { Route, Routes, useNavigate} from 'react-router-dom';
-import Main from './views/Main';
-import CrearPet from './views/CrearPet';
-import PetDetail from './views/PetDetail';
-import EditPet from './views/EditPet';
-import Register from './registro/Register';
-import Login from './registro/Login'
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Login from "./registro/Login";
+import Register from "./registro/Register";
+import CrearPet from "./views/CrearPet";
+import EditPet from "./views/EditPet";
+import Main from "./views/Main";
+import PetDetail from "./views/PetDetail";
 
 function App() {
-
   const navigate = useNavigate();
   const [usuario, setUsuario] = useState();
 
   useEffect(() => {
-    if(!usuario) {
-      if(sessionStorage.getItem('USUARIO')){
-        setUsuario(JSON.parse(sessionStorage.getItem('USUARIO')));
-      }else {
-        navigate('/login');
+    if (!usuario) {
+      if (sessionStorage.getItem("USUARIO")) {
+        setUsuario(JSON.parse(sessionStorage.getItem("USUARIO")));
+      } else {
+        navigate("/login");
       }
     } else {
-      sessionStorage.setItem('USUARIO', JSON.stringify(usuario));
+      sessionStorage.setItem("USUARIO", JSON.stringify(usuario));
     }
-  }, [])
-
+  }, []);
 
   return (
     <React.Fragment>
-      <div style={{backgroundColor:"#7a97cf", width:"100%"}}>
+      <div style={{ backgroundColor: "#7a97cf", width: "100%" }}>
         <Container>
           <Row className="mb-2">
             <Col>
-            <h1 style={{color:"white"}}>Pet Society</h1>
+              <h1 style={{ color: "white" }}>Pet Society</h1>
             </Col>
           </Row>
         </Container>
       </div>
       <Routes>
-        <Route path='/registro' element={<Register/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/*' element={<Main/>}></Route>
-        <Route path='/pets/new' element={<CrearPet/>}></Route>
-        <Route path='/pets/:id' element={<PetDetail/>}></Route>
-        <Route path='/pets/:id/edit' element={<EditPet/>}></Route>
+        <Route path="/registro" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/*" element={<Main />}></Route>
+        <Route path="/pets/new" element={<CrearPet />}></Route>
+        <Route path="/pets/:id" element={<PetDetail />}></Route>
+        <Route path="/pets/:id/edit" element={<EditPet />}></Route>
       </Routes>
     </React.Fragment>
   );
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -2,34 +2,44 @@ import React, { useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 
-import PetCard from "../components/PetCard";
 import axios from "axios";
 import Swal from "sweetalert2";
+import PetCard from "../components/PetCard";
+import PetMap from "../components/PetMap";
 
 const Main = () => {
-
+  const [mapInstance, setMapInstance] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
   const navigate = useNavigate();
   const [listarPet, setListarPet] = useState([]);
 
-    useEffect(() => {
-      axios.get('http://localhost:8000/api/pet')
-      .then(res => {
-        console.log(res.data.datosPet)
-        setListarPet(res.data.datosPet)
-      })
-      }, [])
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/pet").then((res) => {
+      console.log(res.data.datosPet);
+      setListarPet(res.data.datosPet);
+    });
+  }, []);
 
-      const salir = (e) => {
-        sessionStorage.removeItem('USUARIO');
-        Swal.fire({
-          icon: 'success',
-          title: 'Sesión cerrada correctamente',
-        })
-        navigate('/login');
-    }
+  const salir = (e) => {
+    sessionStorage.removeItem("USUARIO");
+    Swal.fire({
+      icon: "success",
+      title: "Sesión cerrada correctamente"
+    });
+    navigate("/login");
+  };
 
+  // const onMapClick = (e) => {
+  //   if (window.confirm(`Are you sure you want to mark this coordinates?`)) {
+  //     setLat(e.latLng.lat());
+  //     setLng(e.latLng.lng());
+  //   }
+  // };
 
-  return(
+  return (
     <React.Fragment>
       <Container>
         <Row className="mb-2">
@@ -37,22 +47,34 @@ const Main = () => {
             <h3>Estas mascotas buscan un nuevo hogar</h3>
           </Col>
           <Col className="d-flex justify-content-end">
-            <Link to='/pets/new'>
-              <Button className="mt-2" variant="primary" >Dar en adopcíon</Button>
+            <Link to="/pets/new">
+              <Button className="mt-2" variant="primary">
+                Dar en adopcíon
+              </Button>
             </Link>
           </Col>
           <Col className="mt-2" md={2}>
-            <Button className='btn btn-danger' onClick={salir}>Cerrar sesión</Button>
+            <Button className="btn btn-danger" onClick={salir}>
+              Cerrar sesión
+            </Button>
           </Col>
         </Row>
         <Row>
           <div className="d-flex flex-wrap">
-              <PetCard datosPet={listarPet}/>
+            <PetCard datosPet={listarPet} />
           </div>
+        </Row>
+        <Row>
+          <Col>
+            <PetMap
+              setSelectedMarker={setSelectedMarker}
+              setMapInstance={setMapInstance}
+            />
+          </Col>
         </Row>
       </Container>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Main;
